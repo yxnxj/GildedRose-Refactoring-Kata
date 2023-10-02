08203b14c6a8adb17ffefb97f4c4dda710775247
조연준 <micayel1219@gmail.com>
2023-10-02 15:46:19 +0900

    test : brie 테스트 작성

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -42,7 +42,7 @@ class GildedRoseTest {
     }
 
     @Test
-    void SulfurasSpend1Day() {
+    void sulfurasSpend1Day() {
         //given
         Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 80) };
         GildedRose app = new GildedRose(items);
@@ -56,6 +56,40 @@ class GildedRoseTest {
         assertEquals("Sulfuras, Hand of Ragnaros", item.name);
         assertEquals(80, item.quality);
         assertEquals(10, item.sellIn);
+    }
+
+    @Test
+    void brieSpend1Day() {
+        //given
+        Item[] items = new Item[] { new Item("Aged Brie", 1, 20) };
+        GildedRose app = new GildedRose(items);
+
+        //when
+        app.updateQuality();
+
+        //then
+        assertNotEquals(0, app.items.length);
+        Item item = app.items[0];
+        assertEquals("Aged Brie", item.name);
+        assertEquals(21, item.quality);
+        assertEquals(0, item.sellIn);
+    }
+
+    @Test
+    void brieSpendAfterSellIn() {
+        //given
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 20) };
+        GildedRose app = new GildedRose(items);
+
+        //when
+        app.updateQuality();
+
+        //then
+        assertNotEquals(0, app.items.length);
+        Item item = app.items[0];
+        assertEquals("Aged Brie", item.name);
+        assertEquals(22, item.quality);
+        assertEquals(-1, item.sellIn);
     }
 
 }
