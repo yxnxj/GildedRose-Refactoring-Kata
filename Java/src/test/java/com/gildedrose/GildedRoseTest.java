8ec1f840293a5b56f4b80aee675c3221059c2ac4
조연준 <micayel1219@gmail.com>
2023-10-02 15:33:11 +0900

    test : 일반 아이템 SellIn 이 지났을 때 테스트 작성

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,15 +3,42 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void normalItemSpend1Day() {
+        //given
+        Item[] items = new Item[] { new Item("normal", 1, 1) };
         GildedRose app = new GildedRose(items);
+
+        //when
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        //then
+        assertNotEquals(0, app.items.length);
+        Item item = app.items[0];
+        assertEquals("normal", item.name);
+        assertEquals(0, item.quality);
+        assertEquals(0, item.sellIn);
+    }
+
+    @Test
+    void normalItemAfterSellIn() {
+        //given
+        Item[] items = new Item[] { new Item("normal", 0, 2) };
+        GildedRose app = new GildedRose(items);
+
+        //when
+        app.updateQuality();
+
+        //then
+        assertNotEquals(0, app.items.length);
+        Item item = app.items[0];
+        assertEquals("normal", item.name);
+        assertEquals(0, item.quality);
+        assertEquals(-1, item.sellIn);
     }
 
 }
