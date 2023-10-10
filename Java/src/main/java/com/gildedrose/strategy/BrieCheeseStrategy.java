a1c70c1ae8715cd30be4642d4d29a11153f5974a
조연준 <micayel1219@gmail.com>
2023-10-10 18:29:48 +0900

    fix : 브리 치즈 sellIn 기한 후 두배 씩 가치 상승

--- Java/src/main/java/com/gildedrose/strategy/BrieCheeseStrategy.java
+++ Java/src/main/java/com/gildedrose/strategy/BrieCheeseStrategy.java
@@ -4,7 +4,9 @@ public class BrieCheeseStrategy implements ItemStrategy {
     private final int defaultPickupRate = 1;
     @Override
     public int updateQuality(int quality, int sellIn) {
-        if (quality < 50){
+        if (sellIn < 0){
+            quality += defaultPickupRate*2;
+        }else {
             quality += defaultPickupRate;
         }
         return quality;
